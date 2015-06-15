626a4c8ab7e343f8fc26e628c2a53c27e594b793
bdorca <bdorca@gmail.con>
2015-06-15 17:26:12 +0200

    Initial commit

---
+++ file_rename.py
@@ -0,0 +1,29 @@
+import argparse
+from os import listdir, rename
+from os.path import isfile, join
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--episodes', help='directory of episodes', required=True)
+    parser.add_argument('-s', '--subtitles', help='subtitle directory', required=True)
+    args = parser.parse_args()
+    print(args.episodes)
+    doit(args.episodes, args.subtitles)
+
+
+def doit(de, ds):
+    episodes = [f for f in listdir(de) if isfile(join(de, f))]
+    subtitles = [f for f in listdir(ds) if isfile(join(ds, f))]
+    episodes.sort()
+    subtitles.sort()
+    if len(episodes) != len(subtitles):
+        exit(1)
+    for x in range(0, len(episodes)):
+        print(episodes[x])
+        print(subtitles[x])
+        rename(ds + "\\" + subtitles[x], ds + "\\" + episodes[x][:-4] + subtitles[x][-4:])
+
+
+if __name__ == '__main__':
+    main()
---
+++ rename_xml.py
@@ -0,0 +1,21 @@
+import argparse
+import json
+import file_rename
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', help="JSON of directories", required=True)
+    args = parser.parse_args()
+    source = args.file
+    with open(source) as source_file:
+        source_json = json.load(source_file)
+        for l in source_json.__getitem__("directories"):
+            d = l["dir"]
+            episodes = d["episodes"]
+            subtitles = d["subtitles"]
+            file_rename.doit(episodes, subtitles)
+
+
+if __name__ == '__main__':
+    main()
